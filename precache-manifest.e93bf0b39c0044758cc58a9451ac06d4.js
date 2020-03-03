a4bf8eb29a4eab424d3a983478a819f7717a96e4
Mark Gallardo <mrk_gllrd@yahoo.com>
2020-03-03 16:49:22 -0600

    Updates

---
+++ precache-manifest.e93bf0b39c0044758cc58a9451ac06d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "718c1da6fa826f77111618c9f174fde8",
+    "url": "/react-game/index.html"
+  },
+  {
+    "revision": "db1dc0b6d9a4de03d188",
+    "url": "/react-game/static/css/main.65975843.chunk.css"
+  },
+  {
+    "revision": "5c28006ef304525d44f8",
+    "url": "/react-game/static/js/2.ca84b911.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-game/static/js/2.ca84b911.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db1dc0b6d9a4de03d188",
+    "url": "/react-game/static/js/main.0aec19e2.chunk.js"
+  },
+  {
+    "revision": "29759790632ab833cdfd",
+    "url": "/react-game/static/js/runtime-main.ffd5b556.js"
+  }
+]);
